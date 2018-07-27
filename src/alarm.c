6f0c89ea17d95f611e1e083872f5e38626fa101d
nigarg99 <nitgarg99@gmail.com>
2018-07-27 19:46:27 -0400

    Embedded python script and gui changes

--- src/alarm.c
+++ src/alarm.c
@@ -1,7 +1,10 @@
 #include "alarm.h"
+#include <Python.h>
+#define PY_FUNC "detect"
 
 static guint _TIMER;
 
+/* Used to pass both the window and the text entry objects */
 struct window_text {
     GtkWidget *window;
     GtkWidget *entry;
@@ -9,6 +12,25 @@ struct window_text {
 
 int main(int argc, char *argv[]) {
 
+    /* Python initialization code */
+    PyObject *pName, *pModule, *pDict, *pFunc, *pValue;
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append(\".\")");
+    
+    /* Insert capture.py into python interpretor */
+    pName = PyString_FromString("capture");
+    pModule = PyImport_Import(pName);
+    pDict = PyModule_GetDict(pModule);
+    pFunc = PyDict_GetItemString(pDict, PY_FUNC);
+    /*if (PyCallable_Check(pFunc))
+    {
+        PyObject_CallObject(pFunc, NULL);
+    } else {
+        PyErr_Print();
+    } */
+
+    /* Set up gui objects */
     GtkWidget *window;
     GtkWidget *entry;
     GtkWidget *vbox;
@@ -24,14 +46,14 @@ int main(int argc, char *argv[]) {
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_container_set_border_width(GTK_CONTAINER(window), 15);
 
-    /* Vertical Box (alignment tool in gtk) */
+    /* Vertical Box */
     vbox = gtk_vbox_new (FALSE, 0);
     gtk_container_add (GTK_CONTAINER(window), vbox);
     gtk_widget_show(vbox);
 
     /* Label */
     label = gtk_label_new("Enter alarm time in minutes then press enter.");
-    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0); //Puts the gtk object into a the 'box' for layout purposes
     gtk_widget_show(label);
 
     /* Text entry box */
@@ -40,23 +62,15 @@ int main(int argc, char *argv[]) {
     wt->window = window;
     wt->entry = entry;
     gtk_entry_set_max_length(GTK_ENTRY(entry), 20);
-    g_signal_connect(entry, "activate", G_CALLBACK(enter_callback), wt);
+    g_signal_connect(entry, "activate", G_CALLBACK(enter_callback), wt); //Set callback function for when user presses 'enter'
     gtk_box_pack_start(GTK_BOX(vbox), entry, TRUE, TRUE, 0);
     gtk_widget_show(entry);
     
-    /* Button 
-    button = gtk_button_new_with_mnemonic("_Button");
-    gtk_widget_set_tooltip_text(button, "Activate onTime");
-    g_signal_connect(button, "clicked",
-            G_CALLBACK(print_msg), NULL);
-    gtk_box_pack_start(GTK_BOX(vbox), button, TRUE, TRUE, 0);
-    gtk_widget_show(button);
-    */
 
     gtk_widget_show(window);
 
     /* Set icon using icon creation function */
-    icon = create_pixbuf("2.png");
+    icon = create_pixbuf("resources/icon.png");
     gtk_window_set_icon(GTK_WINDOW(window), icon);
 
     /* For exiting the window */
@@ -71,6 +85,7 @@ int main(int argc, char *argv[]) {
     gtk_main();
 
     free(wt);
+    g_source_remove(_TIMER);
     return 0;
 }
 
@@ -95,18 +110,8 @@ void print_msg() {
     printf("Button clicked\n");
 }
 
-gboolean alarm_msg(gpointer window) {
+gboolean alarm_msg(gpointer window __attribute__((unused))) {
 
-    /*
-    GtkWidget *dialog;
-    dialog = gtk_message_dialog_new(GTK_WINDOW(window),
-            GTK_DIALOG_DESTROY_WITH_PARENT,
-            GTK_MESSAGE_INFO,
-            GTK_BUTTONS_OK,
-            "Alarm Signaled!");
-    gtk_window_set_title(GTK_WINDOW(dialog), "Signal!");
-    gtk_dialog_run(GTK_DIALOG(dialog));
-    */
     system("notify-send 'Alarm!' 'Move around, you bum.'");
     return TRUE;
 }
@@ -116,11 +121,12 @@ gboolean timer_msg() {
     return TRUE;
 }
 
-void enter_callback(GtkWidget *widget, struct window_text *wt) {
+void enter_callback(GtkWidget *widget __attribute__((unused)), struct window_text *wt) {
     const gchar *entry_text;
     entry_text = gtk_entry_get_text (GTK_ENTRY(wt->entry));
-    g_source_remove(_TIMER);
     int tmp = atoi(entry_text);
+    gtk_entry_set_text(GTK_ENTRY(wt->entry), ""); //Clear the text upon hitting enter
+    g_source_remove(_TIMER);
     tmp = tmp * 60 * 1000; //convert input into ms
     _TIMER = g_timeout_add(tmp, (GSourceFunc) alarm_msg, (gpointer) wt->window);
 }
---
+++ src/capture.py
@@ -0,0 +1,21 @@
+import cv2
+import face_recognition
+from PIL import Image
+
+def detect():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    #grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('test.png', frame)
+    cap.release()
+
+    image = face_recognition.load_image_file("test.png")
+
+    face_locations = face_recognition.face_locations(image)
+
+    print("I found {} face(s) in this image.".format(len(face_locations)))
+    if len(face_locations) > 0:
+        return 1
+    else:
+        return 0
+
